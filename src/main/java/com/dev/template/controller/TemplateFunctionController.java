e8cd57b84d49cc52bdab05064325634953899d3b
yylui <yylui@tsmc.com>
2024-08-09 10:12:35 +0000

    testing for template function api

--- src/main/java/com/dev/template/controller/TemplateFunctionController.java
+++ src/main/java/com/dev/template/controller/TemplateFunctionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -118,7 +119,7 @@ public class TemplateFunctionController {
     @PostMapping(value="/template/uploads", consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
     public RestResult templateUploads(
         @RequestPart("files") List<MultipartFile> files,
-        @RequestPart("description") String description){
+        @RequestParam("description") String description){
         LOGGER.info("========== TemplateController templateUploads START ==========");
         FileUploadRequest fileUploadRequest = new FileUploadRequest();
         fileUploadRequest.setFiles(files);
---
+++ src/test/java/com/example/test/TemplateFunctionControllerTest.java
@@ -0,0 +1,166 @@
+package com.example.test;
+
+import com.dev.template.MicroserviceDemoApplication;
+import com.dev.template.controller.TemplateFunctionController;
+import com.dev.template.dto.PersonRequest;
+import com.dev.constant.CommConstant;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootTest(classes = MicroserviceDemoApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
+public class TemplateFunctionControllerTest {
+
+    @Autowired
+    private TemplateFunctionController templateFunctionController;
+
+    private MockMvc mockMvc;
+	private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(templateFunctionController)  // Replace with actual controller if needed
+                .build();
+		objectMapper = new ObjectMapper(); // inital ObjectMapper
+    }
+
+    @Test
+    public void testTemplateGetPersonRequest() throws Exception {
+		
+		// header setting
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer token");
+		
+		// testing
+		mockMvc.perform(MockMvcRequestBuilders.get("/template/getPersonRequest")
+			.headers(httpHeaders).accept(MediaType.APPLICATION_JSON))
+			// .andDo(MockMvcResultHandlers.print())
+			// .andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.retCode").value(CommConstant.RET_CD_SUCCESS))
+			.andExpect(jsonPath("$.retDetail").value(CommConstant.SUCCESS))
+			.andExpect(jsonPath("$.retObject[0]").exists());
+    }
+
+	@Test
+    public void testTemplateCaulcuteBMI() throws Exception {
+        
+		// input setting
+        PersonRequest personRequest = new PersonRequest();
+        personRequest.setWeight(70);
+        personRequest.setHeight(175);
+		// data to json
+        String personRequestJson = objectMapper.writeValueAsString(personRequest); 
+
+        // header setting
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer token");
+
+        // testing
+        mockMvc.perform(MockMvcRequestBuilders.post("/template/caulcuteBMI")
+                .headers(httpHeaders)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(personRequestJson))
+				// .andDo(MockMvcResultHandlers.print())
+				// .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.retCode").value(CommConstant.RET_CD_SUCCESS))
+                .andExpect(jsonPath("$.retDetail").value(CommConstant.SUCCESS))
+                .andExpect(jsonPath("$.retObject[0].bmi").value(22.857142857142858));
+    }
+
+	@Test
+    public void testTemplateUploads() throws Exception {
+        // input
+        MockMultipartFile file1 = new MockMultipartFile("files", "test1.txt", "text/plain", "sample file content".getBytes());
+        String description = "Sample description";
+
+        // testing
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/template/uploads")
+                .file(file1)
+                .param("description", description)
+                .contentType(MediaType.MULTIPART_FORM_DATA))
+				// .andDo(MockMvcResultHandlers.print())
+				// .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.retCode").value(CommConstant.RET_CD_SUCCESS))
+                .andExpect(jsonPath("$.retDetail").value(CommConstant.SUCCESS))
+                .andExpect(jsonPath("$.retObject[0]").value("test1.txt"));
+    }
+
+	@Test
+    public void testTemplatePutData() throws Exception {
+        
+		// input setting
+        PersonRequest personRequest = new PersonRequest();
+        personRequest.setWeight(70);
+        personRequest.setHeight(175);
+		// data to json
+        String personRequestJson = objectMapper.writeValueAsString(personRequest); 
+
+        // header setting
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer token");
+
+        // testing
+        mockMvc.perform(MockMvcRequestBuilders.put("/template/putData")
+			.headers(httpHeaders)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(personRequestJson))
+			// .andDo(MockMvcResultHandlers.print())
+			// .andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.retCode").value(CommConstant.RET_CD_SUCCESS))
+			.andExpect(jsonPath("$.retDetail").value(CommConstant.SUCCESS));
+    }
+
+	@Test
+    public void testTemplateDeleteData() throws Exception {
+        
+		// input setting
+        PersonRequest personRequest = new PersonRequest();
+        personRequest.setWeight(70);
+        personRequest.setHeight(175);
+		// data to json
+        String personRequestJson = objectMapper.writeValueAsString(personRequest); 
+
+        // header setting
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer token");
+
+        // testing
+        mockMvc.perform(MockMvcRequestBuilders.delete("/template/deleteData")
+			.headers(httpHeaders)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(personRequestJson))
+			// .andDo(MockMvcResultHandlers.print())
+			// .andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.retCode").value(CommConstant.RET_CD_SUCCESS))
+			.andExpect(jsonPath("$.retDetail").value(CommConstant.SUCCESS));
+    }
+}
